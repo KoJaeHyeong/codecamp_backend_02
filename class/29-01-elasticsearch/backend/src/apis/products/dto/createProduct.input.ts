591b5cc0c2eaf7b565d107d26ce1324797fd96bc
KoJaeHyeong <jaehyeong1586@gmail.com>
2022-04-24 18:54:15 +0900

    Nest 29일차

---
+++ class/29-01-elasticsearch/backend/src/apis/products/dto/createProduct.input.ts
@@ -0,0 +1,23 @@
+import { Field, InputType, Int } from '@nestjs/graphql';
+import { ProductSaleslocationInput } from 'src/apis/productsSaleslocation/dto/productSaleslocation.input';
+
+@InputType() // inputtype으로 쓰기위해서 데코레이터 설정!!
+export class createProductInput {
+  @Field(() => String)
+  name: string;
+
+  @Field(() => String)
+  description: string;
+
+  @Field(() => Int)
+  price: number;
+
+  @Field(() => ProductSaleslocationInput)
+  productSaleslocation: ProductSaleslocationInput;
+
+  @Field(() => String)
+  productCategoryId: string;
+
+  @Field(() => [String]) // 스트링 배열 형태
+  productTags: string[]; // 스트링 배열 형태
+}
